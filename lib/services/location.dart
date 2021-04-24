cc65b48df3b56dcc8791d9be21cdd02af99b41e8
Mark <mcarlton33@gmail.com>
2021-04-24 06:32:11 +0200

    added permissions

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -6,25 +6,38 @@ class Locate {
   PermissionsService _permissionsService = PermissionsService();
   
   //Get co-ordinates of device
-  void getCoordinates() async {
+  Future<Map<String, dynamic>> getCoordinates() async {
+    //Check if the permission is granted
     var status = await _permissionsService.requestLocationPermission();
     if (status == true) {
+      //If the location is granted. Check if the GPS is on
       Location _locationService = Location();
+      //Show the GPS dialog
       bool status = await _locationService.requestService();
+      //If the GPS is on, get the location data
       if (status) {
-        await _locationService.changeSettings(accuracy: LocationAccuracy.HIGH, interval: 1000);
+        await _locationService.changeSettings(accuracy: LocationAccuracy.HIGH, interval: 100);
         //Get Location Data
         LocationData location;
         location = await _locationService.getLocation();
         var lat = location.latitude;
-        print(lat);
-        var long = location.longitude;
-        print(long);
+        print('Lat - $lat');
+        var lon = location.longitude;
+        print('Lon - $lon');
+        Map<String, dynamic> coords = {
+          'lat': lat,
+          'lon': lon
+        };
+        return coords;
+      }
+      else {
+        return null;
       }
     }
     else {
       //Executed if permission is not granted
       _permissionsService.requestLocationPermission();
+      return null;
     }
   }
 }
